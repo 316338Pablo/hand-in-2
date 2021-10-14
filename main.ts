9ecaf1faa793db2a422ec4f364567033ac351993
316338Pablo <91956178+316338Pablo@users.noreply.github.com>
2021-10-14 13:36:22 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+game.showLongText("A humble carpenter has been working for years so that he could create something unique,something that will amaze the world. ", DialogLayout.Bottom)
+game.showLongText("His masterpiece was a table made with the best quality wood. ", DialogLayout.Bottom)
+game.showLongText("Once he finished his masterpiece, the table started fleeing from home, something that annoyed toa high extent the carpenter. ", DialogLayout.Bottom)
+game.showLongText("For this reason, he will hunt the table ", DialogLayout.Bottom)
+game.showLongText("FOREVER", DialogLayout.Bottom)
